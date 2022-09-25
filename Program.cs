ca26c0f6929fc7636c8ebdd8d9b74cb7efd60a53
MasterSlicer69 <hajlerzoltan@gmail.com>
2022-09-25 12:30:06 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smoke_Basin
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				StreamReader sr = new StreamReader("data.txt");
+				string line = sr.ReadLine();
+				Int64.Parse(line);
+				sr.re
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+			
+			int input = int.Parse(Console.ReadLine());
+			int[,] matrixData = new int[input,0];
+			for (int i = 0; i < matrixData.GetLength(0); i++) {
+				for (int j = 0; j < matrixData.GetLength(1); j++)
+				{
+					Console.Write(matrixData[i, j]);
+				}
+				Console.WriteLine();
+			}
+			Console.ReadKey();
+		}
+	}
+}
